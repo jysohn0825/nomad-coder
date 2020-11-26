527c400c7b46689ae81499db94a22b6602ab3c7d
RB_ <jysohn0825@naver.com>
2020-11-26 14:43:14 +0900

    Add files via upload

--- Kokoa_Clone_WebUI/Kokoa_Clone_WebUI_v2/app/app.py
+++ Kokoa_Clone_WebUI/Kokoa_Clone_WebUI_v2/app/app.py
@@ -1,11 +1,10 @@
-import requests
 import pymysql
-from flask import Flask, render_template, jsonify, request, session, url_for, redirect
+import datetime
+from flask import Flask, render_template, request, session, url_for, redirect
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
 
-## HTML을 주는 부분
 @app.route('/')
 def index() :
     if 'username' in session :
@@ -16,7 +15,6 @@ def index() :
             return redirect(url_for('index'))
     return render_template('/index.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login() :
     name = request.form['username']
@@ -24,7 +22,7 @@ def login() :
     conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
     cur = conn.cursor()
     try:
-        sql = "Select * from user where USER_ID = '" + name + "'"
+        sql = "SELECT * FROM USER WHERE USER_ID = '" + name + "'"
         cur.execute(sql)
         userinfo = cur.fetchone()
         if name == '' or None :
@@ -50,8 +48,14 @@ def signup() :
     id = request.form['username']
     pwd = request.form['password']
     email = request.form['email']
-    print(name, id, pwd, email)
+    conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cur = conn.cursor()
+    sql = "INSERT USER VALUES('" + id + "', '" + name + "', '" + pwd + ", null, null, null, '" + email + "')"
+    cur.execute()
+    conn.commit()
     session['username'] = id
+    cur.close()
+    conn.close()
     return redirect(url_for('friends'))
 
 @app.route('/logout')
@@ -64,18 +68,24 @@ def friends() :
     username = session['username']
     conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
     cur = conn.cursor()
-    sql = "SELECT * FROM user WHERE USER_ID IN (SELECT USER2 FROM friends WHERE USER1 = '" + username + "')"
+    sql = "SELECT * FROM USER WHERE USER_ID IN (SELECT USER2 FROM FRIENDS WHERE USER1 = '" + username + "')"
     sql+= " UNION "
-    sql+= "SELECT * FROM user WHERE USER_ID IN (SELECT USER1 FROM friends WHERE USER2 = '" + username + "')"
+    sql+= "SELECT * FROM USER WHERE USER_ID IN (SELECT USER1 FROM FRIENDS WHERE USER2 = '" + username + "')"
     cur.execute(sql)
     fList = list(cur.fetchall())
     fList.sort()
-    sql = "SELECT * FROM user WHERE USER_ID = '" + username + "'"
+    sql = "SELECT * FROM USER WHERE USER_ID = '" + username + "'"
     cur.execute(sql)
     my = cur.fetchone()
+
+    sql = "SELECT COUNT(CASE WHEN CHAT_FLAG = 1 THEN 1 END)"
+    sql+= "FROM CHAT WHERE CHAT.USER_ID != '"+username+"'"
+    cur.execute(sql)
+    count = cur.fetchone()[0]
+
     cur.close()
     conn.close()
-    return render_template('/friends.html', my = my, fList = fList)
+    return render_template('/friends.html', my = my, fList = fList, count = count)
 
 @app.route('/changeProfile', methods=['POST'])
 def changeProfile():
@@ -84,7 +94,7 @@ def changeProfile():
     comment = request.form['comment']
     conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
     cur = conn.cursor()
-    sql = "UPDATE user SET USER_NAME = '" + name + "', USER_SELF = '" + comment + "' WHERE USER_ID = '" + username + "'"
+    sql = "UPDATE USER SET USER_NAME = '" + name + "', USER_SELF = '" + comment + "' WHERE USER_ID = '" + username + "'"
     cur.execute(sql)
     conn.commit()
     cur.close()
@@ -96,7 +106,7 @@ def profile(user) :
     username = session['username']
     conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
     cur = conn.cursor()
-    sql = "SELECT * FROM user WHERE USER_ID = '" + user + "'"
+    sql = "SELECT * FROM USER WHERE USER_ID = '" + user + "'"
     cur.execute(sql)
     userProfile = cur.fetchone()
     cur.close()
@@ -107,7 +117,7 @@ def profile(user) :
 def image(user) :
     conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
     cur = conn.cursor()
-    sql = "SELECT * FROM user WHERE USER_ID = '" + user + "'"
+    sql = "SELECT * FROM USER WHERE USER_ID = '" + user + "'"
     cur.execute(sql)
     userProfile = cur.fetchone()
     cur.close()
@@ -120,7 +130,7 @@ def edit(user) :
     username = session['username']
     conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
     cur = conn.cursor()
-    sql = "SELECT * FROM user WHERE USER_ID = '" + user + "'"
+    sql = "SELECT * FROM USER WHERE USER_ID = '" + user + "'"
     cur.execute(sql)
     userProfile = cur.fetchone()
     cur.close()
@@ -128,34 +138,134 @@ def edit(user) :
     return render_template('/edit.html', userProfile = userProfile)
 
 @app.route('/chats', methods=['GET'])
-def channel() :
-    return render_template('/chats.html')
+def chats() :
+    username = session['username']
+    conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cur = conn.cursor()
+    sql = "SELECT CR.ROOM, CR.USER, USER.USER_NAME, CR.NONREAD, CR.LATEST, USER.USER_PIC "
+    sql+= "FROM USER, (SELECT CHATROOM.ROOM_ID AS ROOM, CASE  WHEN USER1 = '"+username+"' THEN USER2 ELSE USER1 END AS USER, "
+    sql+= "COUNT(CASE WHEN CHAT_FLAG = 1 AND USER_ID != '"+username+"' THEN 1 END) AS NONREAD, MAX(SUBSTRING(CHAT_TIME,1,16)) AS LATEST "
+    sql+= "FROM CHAT, CHATROOM "
+    sql+= "WHERE CHATROOM.ROOM_ID = 1) AS CR "
+    sql+= "WHERE CR.USER = USER.USER_ID"
+    cur.execute(sql)
+    rList = list(cur.fetchall())
+    rList.sort()
 
-@app.route('/chat', methods=['GET'])
-def ch1() :
-    return render_template('/chat.html')
+    sql = "SELECT COUNT(CASE WHEN CHAT_FLAG = 1 THEN 1 END)"
+    sql+= "FROM CHAT WHERE CHAT.USER_ID != '"+username+"'"
+    cur.execute(sql)
+    count = cur.fetchone()[0]
+
+    cur.close()
+    conn.close()
+    return render_template('/chats.html', rList = rList, count = count)
+
+@app.route('/findRoom/<userID>', methods = ['GET'])
+def findRoom(userID):
+    username = session['username']
+    conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cur = conn.cursor()
+    sql = "SELECT ROOM_ID FROM CHATROOM "
+    sql+= "WHERE (USER1 = '"+userID+"' AND USER2 = '"+username+"') OR (USER1 = '"+username+"' AND USER2 = '"+userID+"')"
+    cur.execute(sql)
+    temp = cur.fetchone()
+    if temp is None:
+        sql = "INSERT INTO CHATROOM (USER1, USER2) values ('"+username+"', '"+userID+"');"
+        cur.execute(sql)
+        conn.commit()
+        sql = "SELECT ROOM_ID FROM CHATROOM "
+        sql += "WHERE (USER1 = '" + userID + "' AND USER2 = '" + username + "') OR (USER1 = '" + username + "' AND USER2 = '" + userID + "')"
+        cur.execute(sql)
+        chatnum = str(cur.fetchone()[0])
+        pass
+    else:
+        chatnum = str(temp[0])
+    cur.close()
+    conn.close()
+    return redirect("/../chat/" + chatnum)
+
+@app.route('/chat/<chatnum>/send', methods=['POST'])
+def chatSend(chatnum):
+    username = session['username']
+    message = request.form['send']
+
+    now = datetime.datetime.now()
+    nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S')
+
+    conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cur = conn.cursor()
+    sql = "INSERT INTO CHAT VALUES (null,'"+nowDatetime+"','"+ username + "','"+message+"',"+str(chatnum)+",1)"
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect("../../chat/"+chatnum)
+
+@app.route('/chat/<chatnum>/read', methods=['GET'])
+def readChat(chatnum):
+    username = session['username']
+    conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cur = conn.cursor()
+    sql = "UPDATE CHAT SET CHAT_FLAG = 0 WHERE USER_ID != '"+username+"' AND ROOM_ID = "+chatnum
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    conn.close()
+    return redirect("../../chat/"+chatnum)
+
+@app.route('/chat/<chatnum>', methods=['GET'])
+def chat(chatnum) :
+    username = session['username']
+    conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cur = conn.cursor()
+    sql = "SELECT CHAT.USER_ID, USER_NAME, CHAT_LOG, SUBSTRING(CHAT_TIME,11,6), USER_PIC "
+    sql+= "FROM CHAT, USER "
+    sql+= "WHERE ROOM_ID = '"+chatnum+"' AND USER.USER_ID = CHAT.USER_ID "
+    sql+= "ORDER BY CHAT_TIME"
+    cur.execute(sql)
+    cList = list(cur.fetchall())
+    sql = "SELECT * FROM CHATROOM "
+    sql+= "WHERE (USER1 = '"+ username + "' OR USER2 = '"+ username + "') AND ROOM_ID = "+chatnum
+    cur.execute(sql)
+    rName = cur.fetchone()
+    cur.close()
+    conn.close()
+    return render_template('/chat.html', username = username, chatnum = chatnum, cList = cList, rName = rName)
 
 @app.route('/find', methods=['GET'])
 def fin() :
-    return render_template('/find.html')
+    username = session['username']
+    conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
+    cur = conn.cursor()
+    sql = "SELECT COUNT(CASE WHEN CHAT_FLAG = 1 THEN 1 END)"
+    sql+= "FROM CHAT WHERE CHAT.USER_ID != '"+username+"'"
+    cur.execute(sql)
+    count = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return render_template('/find.html', count = count)
 
 @app.route('/more', methods=['GET'])
 def mor() :
     username = session['username']
     conn = pymysql.connect(host=IP, user=USER, password=PASSWORD, db=DB, charset='utf8')
     cur = conn.cursor()
-    sql = "SELECT * FROM user WHERE USER_ID = '" + username + "'"
+    sql = "SELECT * FROM USER WHERE USER_ID = '" + username + "'"
     cur.execute(sql)
     userProfile = cur.fetchone()
+    sql = "SELECT COUNT(CASE WHEN CHAT_FLAG = 1 THEN 1 END)"
+    sql+= "FROM CHAT WHERE CHAT.USER_ID != '"+username+"'"
+    cur.execute(sql)
+    count = cur.fetchone()[0]
     cur.close()
     conn.close()
-    return render_template('/more.html',userProfile = userProfile)
+    return render_template('/more.html',userProfile = userProfile, count = count)
 
 @app.route('/settings', methods=['GET'])
 def set() :
     return render_template('/settings.html')
 
-## 전역 변수부
 
 # DB 관련
 conn, cur = None, None
